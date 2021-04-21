88dd02a450e1d2894e8a117e410e42ea78871ad7
Leonidas P. Papadakos <papadakospan@gmail.com>
2021-04-21 21:16:46 +0300

    Add code to loop though input files

--- Main.java
+++ Main.java
@@ -1,3 +1,37 @@
-public class Main {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
+import syntaxtree.*;
+import visitor.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		if (args.length < 1) {
+			System.err.println("You need to pass arguments like so: <inputFile1> <inputfile2> ... <inputFileN>");
+			System.exit(1);
+		}
+
+		for (String file: args) {
+			FileInputStream input = null;
+
+			try {
+				input = new FileInputStream(file);
+				Goal root = new MiniJavaParser(input).Goal();
+
+				System.out.println("Apparently, this program was parsed correctly... Huh.");
+
+				// TODO: this is where visitor stuff would go
+			} catch(Exception e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					if (input != null)
+						input.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 }
--- Makefile
+++ Makefile
@@ -3,11 +3,7 @@ all:
 	java -jar javacc5.jar minijava-jtb.jj
 	javac Main.java
 
-run: execute
-execute:
-	java Main
-
 clean:
 	rm -f sytaxtree/ visitor/ *~ *.class JavaCharStream.java  Mini*.java ParseException.java Token*.java minijava-jtb.jj
 
-.PHONY: all run execute clean
+.PHONY: all clean
